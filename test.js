52735fad44a59829c7bb27c9c4c505c4a4395378
Alexander Pavlyukov <tanzoor@gmail.com>
2013-09-17 15:09:43 +0400

    testing

--- test.js
+++ test.js
@@ -4,7 +4,5 @@ function changeColor() {
 	for (var index in RGBArray) {
 		RGBArray[index] = Math.floor(Math.random()*255);
 	}
-	console.log(RGBArray[0]);
-	console.log(RGBArray[1]);
-	console.log(RGBArray[2]);
+	console.log(RGBArray);
 }
