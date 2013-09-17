011bc8e48ba004a95a276ea03cc2df2bc16f55e9
Alexander Pavlyukov <tanzoor@gmail.com>
2013-09-17 14:13:46 +0400

    testing

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 function test() {
 	alert('script is ok!');
 	var currentColor = $('body').css('background-color');
-	$('#button p span').html(currentColor);
+	console.log(currentColor);
+	$('span').html(currentColor);
 }
