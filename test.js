58f81b6e1723f593b2c3c9d2ba214a17bc7d5f56
Alexander Pavlyukov <tanzoor@gmail.com>
2013-09-17 14:44:18 +0400

    testing

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function test() {
 	alert('script is ok!');
-	$('input').click(function() {
-		/*var currentColor = $('body').css('background-color');
+	/*$('input').click(function() {
+		var currentColor = $('body').css('background-color');
 		console.log(currentColor);
-		$('#color').html(currentColor);*/
+		$('#color').html(currentColor);
 		alert('button click');
-	});
+	});*/
 }
