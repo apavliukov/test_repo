c48fde11732a673922577e9cff9981fb52fb42bb
tanzoor <tanzoor@gmail.com>
2013-09-18 11:22:13 +0400

    working

--- projects/random_color/script.js
+++ projects/random_color/script.js
@@ -1,24 +1,27 @@
 var RGBArray = [0, 0, 0];
+var HEXArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 function changeColor() {
-	for (var index in RGBArray) {
-		RGBArray[index] = Math.floor(Math.random()*255);
+	for (var i in RGBArray) {
+		RGBArray[i] = Math.floor(Math.random()*255);
 	}
 	var newColor = 'rgb(' + RGBArray[0] + ', ' + RGBArray[1] + ', ' + RGBArray[2] + ')';
 	$('body').css('background-color', newColor);
 	$('#color').html(newColor);
 }
 
-function isInteger(num) {
-  return (num ^ 0) === num;
-}
-
-function convertNumber(number, systemTo) {
+function convertDecToHex(number) {
 	var mods = [];
 	var result = 0;
 	while (number > 0) {
-		mods.push(number % systemTo);
+		mods.push(number % 16);
 		number = Math.floor(number / 16);
+	}
+	var len = mods.length;
+	for (var i in mods) {
+		var temp = mods[i];
+		mods[i] = mods[len-i-1];
+		mods[len-i-1] = temp;
 	}
 	console.log(mods);
 }
@@ -28,5 +31,5 @@ function main() {
 	$('button').click(function() {
 		changeColor();
 	});
-	convertNumber(255, 16);
+	convertDecToHex(255, 16);
 }
