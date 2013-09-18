6406e6945078e89b739952ad9ae1091552d6aadc
tanzoor <tanzoor@gmail.com>
2013-09-18 10:58:43 +0400

    fixing

--- projects/random_color/script.js
+++ projects/random_color/script.js
@@ -12,7 +12,7 @@ function changeColor() {
 function convertNumber(number, systemTo) {
 	var mods = [];
 	var result = 0;
-	while (number > 0) {
+	while (isInteger(number) && number > 0) {
 		var mod = number % systemTo;
 		mods.push(mod);
 		number = number / 16;
